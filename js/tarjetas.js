98b33946fc7a5d8542c1dd25c04eaf7cecb9885c
Xiang <xiang1016127637@gmail.com>
2018-05-04 01:07:51 +0200

    Guiones tarjetas

--- js/tarjetas.js
+++ js/tarjetas.js
@@ -48,4 +48,11 @@ $(document).ready(function() {
             $('#input_tarjeta').addClass("is-invalid");
         }
     });
+    $('#input_tarjeta').keyup(function() {
+      var foo = $(this).val().split("-").join(""); // remove hyphens
+      if (foo.length > 0) {
+        foo = foo.match(new RegExp('.{1,4}', 'g')).join("-");
+      }
+      $(this).val(foo);
+    });
 });
